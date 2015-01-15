b30bf0ff2183a305fc863407f7542bc166e2d3d1
shakadak <nathanael.pineau@gmail.com>
2015-01-15 18:15:46 +0100

    testing some linking

--- C/src/main.c
+++ C/src/main.c
@@ -6,11 +6,19 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/15 15:57:48 by npineau           #+#    #+#             */
-/*   Updated: 2015/01/15 15:58:40 by npineau          ###   ########.fr       */
+/*   Updated: 2015/01/15 18:14:52 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "libft.h"
+
+int main(int argc, char **argv)
 {
+	(void)argv;
+	if (argc != 2)
+	{
+		ft_putendl_fd("Usage: ./wolf3d path/to/map", 2);
+		return (1);
+	}
 	return (0);
 }
