cddc3f06419a81f425cb1ef26a890781bf5e5f45
shakadak <nathanael.pineau@gmail.com>
2015-01-15 19:09:12 +0100

    un

---
+++ C/inc/wolf3d.h
@@ -0,0 +1,7 @@
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+#include <keysymdef.h>
+#include <mlx.h>
+
+#endif
---
+++ C/src/window_initialization.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window_initialization.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/15 18:42:08 by npineau           #+#    #+#             */
+/*   Updated: 2015/01/15 18:42:24 by npineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf3d.h"
+
+void	*init()
+{
+	return (mlx_init());
+}
+
+void	*new_window(void *env)
+{
+	return (mlx_new_window(env, W_WIDTH, W_HEIGHT, W_TITLE));
+}
