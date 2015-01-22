6d8cbb1d8953264d42d00e0494be252590bbf8c4
shakadak <nathanael.pineau@gmail.com>
2015-01-22 14:01:28 +0100

    adding line drawing

--- C/libmlx/inc/libmlx.h
+++ C/libmlx/inc/libmlx.h
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 11:26:29 by npineau           #+#    #+#             */
-/*   Updated: 2015/01/22 11:28:18 by npineau          ###   ########.fr       */
+/*   Updated: 2015/01/22 13:34:08 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 #include <mlx.h>
 #include <string.h>
 
-typedef struct		s_point
+typedef struct		s_pos
 {
 	int				x;
 	int				y;
 	int				z;
-}					t_point;
+}					t_pos;
 
 typedef union		u_color
 {
@@ -29,11 +29,11 @@ typedef union		u_color
 	unsigned char	rgb[4];
 }					t_color;
 
-typedef struct		s_pixel
+typedef struct		s_pix
 {
-	t_point			coordinate;
+	t_pos			pos;
 	t_color			color;
-}					t_pixel;
+}					t_pix;
 
 typedef struct		s_img
 {
@@ -58,7 +58,9 @@ void				*new_mlx(void);
 t_img				new_image(void *mlx, int width, int height);
 t_win				new_window(void *mlx, int width, int height, char *title);
 
-void				put_pixel_to_image(t_img *img, t_pixel const pixel);
+void				put_pixel_to_image(t_img *img, t_pix const pixel);
+void				clear_image(t_img *img);
+void				draw_line(t_img *img, t_pix start, t_pix end)
 
 void				ft_fatal(char const *const msg, int const flag);
 
--- C/libmlx/src/pix/sources.mk
+++ C/libmlx/src/pix/sources.mk
@@ -1,5 +1,6 @@
 PIX	:=	put_pixel_to_image.c \
-		clear_image.c
+		clear_image.c \
+		draw_line.c
 
 OBJ := $(OBJ) $(PIX:.c=.o)
 
