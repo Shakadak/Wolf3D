e11fb86d4960287e35ee5fc252b690f35113de37
shakadak <nathanael.pineau@gmail.com>
2015-01-28 14:59:40 +0100

    straffing is easy

--- C/src/main/movement.c
+++ C/src/main/movement.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/23 17:55:41 by npineau           #+#    #+#             */
-/*   Updated: 2015/01/27 17:45:34 by npineau          ###   ########.fr       */
+/*   Updated: 2015/01/28 14:58:01 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 static void	rightward(t_player *c, t_map const map, double speed)
 {
 	if (!check(c->pos.x + c->dir.y * speed, c->pos.y, map))
-		c->pos.x += fabs(c->dir.y) * speed;
+		c->pos.x += -c->dir.y * speed;
 	if (!check(c->pos.x, c->pos.y - c->dir.x * speed, map))
-		c->pos.y += fabs(c->dir.x) * speed;
+		c->pos.y += c->dir.x * speed;
 }
 
 static void	leftward(t_player *c, t_map const map, double speed)
 {
 	if (!check(c->pos.x + c->dir.y * speed, c->pos.y, map))
-		c->pos.x -= fabs(c->dir.y) * speed;
+		c->pos.x += c->dir.y * speed;
 	if (!check(c->pos.x, c->pos.y + c->dir.x * speed, map))
-		c->pos.y -= fabs(c->dir.x) * speed;
+		c->pos.y += -c->dir.x * speed;
 }
 
 static void	forward(t_player *c, t_map const map, double speed)
